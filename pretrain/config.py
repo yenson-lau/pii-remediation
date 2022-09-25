e6da490f3b96054e286e4550dd4a8f8e2ce83e7b
Yenson Lau <yenson.lau@gmail.com>
2022-09-24 20:06:47 -0400

    pretrain.config

--- pretrain/config.py
+++ pretrain/config.py
@@ -8,20 +8,20 @@ __DIR__ = path.dirname(path.realpath(__file__))
 class Config:
     config: DictConfig
 
-    def set_config(config: Union[str, DictConfig]):
+    def reset_config(config: Optional[Union[str, DictConfig]] = None) -> None:
+        if config is None:  config = path.join(__DIR__, "config.yaml")
         Config.config = OmegaConf.load(config) if isinstance(config, str) else config
 
-    def get_exp_dir():
-        config = Config.config
-        return path.relpath(path.join(config.data_dir.format(__dir__=__DIR__),
-                                      config.exp_label))
+    def resolve_config(as_dict:bool = False) -> None:
+        Config.config = Config.resolve_subdirs(Config.config, as_dict=as_dict)
 
-    def resolve(
+    def resolve_subdirs(
         subconfig: Optional[Union[dict, DictConfig]] = None,
         as_dict: bool = False
     ) -> Union[dict, DictConfig]:
 
-        exp_dir = Config.get_exp_dir()
+        exp_dir = path.relpath(path.join(config.data_dir.format(__dir__=__DIR__),
+                                         config.exp_label))
         subconfig = (Config.config if subconfig is None else subconfig).copy()
 
         for k, v in subconfig.items():
@@ -34,4 +34,5 @@ class Config:
 
         return OmegaConf.to_object(subconfig) if as_dict else subconfig
 
-Config.set_config(path.join(__DIR__, "config.yaml"))
+
+Config.reset_config()
