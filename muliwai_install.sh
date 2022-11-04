6e29cd0312422d24d31fa8d86678fbda000f30cd
Yenson Lau <yenson.lau@gmail.com>
2022-11-04 13:46:38 -0400

    Update muliwai_install.sh

--- muliwai_install.sh
+++ muliwai_install.sh
@@ -4,8 +4,10 @@ git submodule add https://github.com/yenson-lau/muliwai
 git submodule update --init
 
 pip install https://github.com/kpu/kenlm/archive/master.zip
-pip install spacy==3.1.0 dateparser python-stdnum protobuf==3.20.1 cdifflib datasets langid faker sentencepiece fsspec tqdm sentence-transformers nltk tokenizers==0.11.3
-pip install --upgrade transformers
+pip install dateparser python-stdnum protobuf==3.20.1 cdifflib datasets langid faker sentencepiece fsspec tqdm nltk
+pip install spacy==3.1.0 sentence-transformers==0.2.5.1 tokenizers==0.11.3 transformers==4.24.0
+# pip install --upgrade transformers
+
 python -m nltk.downloader punkt wordnet
 python -m spacy download en_core_web_sm
 python -m spacy download fr_core_news_sm
